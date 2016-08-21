3375f03ae58f0a9aff3e522f1134f0a54ec65a0c
codephillip <codephillip@gmail.com>
2016-08-21 13:57:17 +0300

    passed movie to download page

--- movie/views.py
+++ movie/views.py
@@ -4,13 +4,15 @@ from movie.models import Movie
 
 
 def index(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.all()[:12]
     return render(request, 'index.html', {
         'movies': movies
     })
 
 
 def download(request, pk):
+    movie = Movie.objects.get(pk=pk)
     return render(request, 'download.html', {
-        'pk': pk
+        'pk': pk,
+        'movie': movie
     })
