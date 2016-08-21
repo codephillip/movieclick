a76a5bbf9fbdada44ebad8d87ae95f5430b10634
codephillip <codephillip@gmail.com>
2016-08-21 14:09:01 +0300

    added link to movie, tested download :)

--- movie/admin.py
+++ movie/admin.py
@@ -5,7 +5,7 @@ from movie.models import Movie, Category
 
 class MovieAdmin(admin.ModelAdmin):
     model = Movie
-    list_display = ('id', 'name', 'category', 'image')
+    list_display = ('id', 'name', 'category', 'link', 'image')
 
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Category)
--- movie/models.py
+++ movie/models.py
@@ -13,6 +13,7 @@ class Category(models.Model):
 class Movie(models.Model):
     name = models.CharField(max_length=200)
     image = models.CharField(max_length=400)
+    link = models.CharField(max_length=400, default='http://localhost/downloads/baxster_admin_panel.zip')
     category = models.ForeignKey(Category)
 
     def __unicode__(self):
