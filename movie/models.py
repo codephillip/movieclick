f92db9976fe6b6c1bc56e19c5d4b663c475b905a
codephillip <codephillip@gmail.com>
2016-08-21 13:02:05 +0300

    added movie and category to models

--- movie/models.py
+++ movie/models.py
@@ -2,4 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=400)
+    category = models.ForeignKey(Category)
+
+    def __unicode__(self):
+        return self.name
+
