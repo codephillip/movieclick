b9cd1cef1e0ae3d42b819bfcc9afaafcd33d1ae9
codephillip <codephillip@gmail.com>
2016-08-21 12:52:32 +0300

    added download page

--- movie/views.py
+++ movie/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def download(request, pk):
+    return render(request, 'download.html', {
+        'pk': pk
+    })
--- movieclick/urls.py
+++ movieclick/urls.py
@@ -6,4 +6,5 @@ from movie import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^download/(?P<pk>[-\w]+)/$', views.download, name='download'),
 ]
