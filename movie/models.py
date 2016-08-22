de52711b16dd98a53ebddcdc3392bb9b9afb80c8
codephillip <codephillip@gmail.com>
2016-08-22 23:27:03 +0300

    finished browse pages

--- movie/models.py
+++ movie/models.py
@@ -11,8 +11,8 @@ class Category(models.Model):
 
 
 class Movie(models.Model):
-    name = models.CharField(max_length=200)
-    image = models.CharField(max_length=400)
+    name = models.CharField(max_length=200, default='Planet of the Apes')
+    image = models.CharField(max_length=400, default='http://localhost/images/gridallbum3.png')
     link = models.CharField(max_length=400, default='http://localhost/downloads/movie1.zip')
     category = models.ForeignKey(Category)
 
--- movie/views.py
+++ movie/views.py
@@ -2,9 +2,27 @@ from django.shortcuts import render
 
 from movie.models import Movie
 
+next_limit = 60
+
 
 def index(request):
     movies = Movie.objects.all()[:12]
+    return render(request, 'index.html', {
+        'movies': movies
+    })
+
+
+def browse(request, pk):
+    global next_limit
+    end = int(pk)
+    if end == 1:
+        movies = Movie.objects.all()[:12]
+        next_limit = 60
+    elif end == 60:
+        movies = Movie.objects.all()[next_limit: next_limit + 12]
+        next_limit += 12
+    else:
+        movies = Movie.objects.all()[pk: end + 12]
     return render(request, 'index.html', {
         'movies': movies
     })
