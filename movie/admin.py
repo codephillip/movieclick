8e6706d8772ab06169c680a4ff052b643a751ff0
codephillip <codephillip@gmail.com>
2016-08-27 23:42:23 +0300

    added vote_average to admin.py

--- movie/admin.py
+++ movie/admin.py
@@ -5,7 +5,7 @@ from movie.models import Movie, Category
 
 class MovieAdmin(admin.ModelAdmin):
     model = Movie
-    list_display = ('id', 'name', 'category', 'download_link', 'watch_link')
+    list_display = ('id', 'name', 'category', 'vote_averate', 'download_link', 'watch_link')
 
 
 class CategoryAdmin(admin.ModelAdmin):
